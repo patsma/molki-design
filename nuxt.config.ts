9714d7bc571e14c623edd771d1ff27a21de07813
Patryk Smakosz <patryksmakosz1@gmail.com>
2025-01-20 03:55:18 +0100

    chore: netlify fix?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,14 +6,14 @@ export default defineNuxtConfig({
     host: "0.0.0.0",
     port: 3000,
   },
-  // postcss: {
-  //     plugins: {
-  //         tailwindcss: {},
-  //         autoprefixer: {},
-  //         cssnano:
-  //             process.env.NODE_ENV === 'production'
-  //                 ? { preset: ['default', { discardComments: { removeAll: true } }] }
-  //                 : false, // disable cssnano when not in production
-  //     },
-  // },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+      cssnano:
+        process.env.NODE_ENV === "production"
+          ? { preset: ["default", { discardComments: { removeAll: true } }] }
+          : false, // disable cssnano when not in production
+    },
+  },
 });

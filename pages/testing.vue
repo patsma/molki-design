d2c6246ac5befd0579b8246be776fb096b56b829
Patryk Smakosz <patryksmakosz1@gmail.com>
2025-01-20 03:56:44 +0100

    refactor: simplify testing page layout and remove unused animation logic

--- pages/testing.vue
+++ pages/testing.vue
@@ -1,41 +1,5 @@
-<script setup lang="ts">
-definePageMeta({
-  layout: 'test'
-})
-import { onMounted, ref, watch } from 'vue';
-import { gsap, TimelineLite } from 'gsap'; // Import the TimelineLite or appropriate class
-import { useUserStore } from '~/stores/user';
-
-const user = useUserStore();
-const textRef = ref(null);
-
-// Explicitly declare the type of timeline
-let timeline: GSAPTimeline | null = null; // or GSAPTimeline or TimelineMax
-
-onMounted(() => {
-  timeline = gsap.timeline({ paused: true })
-      .from(textRef.value, { duration: 1, y: 50, opacity: 0 });
-});
-
-watch(() => user.isAnimationPlaying, (newVal) => {
-  if (newVal && timeline) {
-    timeline.play();
-  } else if (timeline) {
-    timeline.reverse();
-  }
-});
-
-</script>
-
-
 <template>
-  <div>
-    <NuxtLink to="/">Home</NuxtLink>
-    <h1>Testing</h1>
-    <p  class="text-to-be-animated">Lorem ipsum...</p>
-    <p v-if="user.isLoggedIn">Hello, {{ user.displayName }}</p>
-    <button ref="textRef" @click="user.logIn('John')">Log In</button>
-    <button @click="user.logOut" v-if="user.isLoggedIn">Log Out</button>
-    <button @click="user.toggleAnimation">Toggle Animation</button>
+  <div class="bg-red min-h-screen">
+    <h1 class="text-4xl font-bold my-8">Testing</h1>
   </div>
 </template>

00965a736e02f5ee18e1e75a686ff8b556960c3f
Patryk Smakosz <patryksmakosz1@gmail.com>
2025-01-29 13:13:50 +0100

    fix: update MainMenu link and todo list
    - Corrected MainMenu link to use absolute path for 'PROJEKT WNĘTRZA'
    - Updated todo list with completed task for typescript build error

--- components/MainMenu.vue
+++ components/MainMenu.vue
@@ -17,7 +17,7 @@ const menuItems = [
   },
   {
     label: 'PROJEKT WNĘTRZA',
-    link: '#spacer1',
+    link: '/#spacer1',
     children: [
       { label: 'REALIZACJE', link: '#projekty-realizacje' },
       { label: 'CENNIK', link: '#cennik-projekt' },

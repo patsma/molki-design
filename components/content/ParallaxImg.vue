e711c9e1587d86cc5331cc07545e0cc23e0bc2c3
Patryk Smakosz <patryksmakosz1@gmail.com>
2025-02-10 15:11:47 +0100

    feat(content): expand homepage with detailed sections and team information

--- components/content/ParallaxImg.vue
+++ components/content/ParallaxImg.vue
@@ -11,11 +11,10 @@
 <style lang="scss">
 .parallax-wrapper {
   height: 120%;
-}
-
-.parallax-content {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 }
 </style>

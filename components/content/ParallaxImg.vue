a31b019c345bdb125377da2f38aaa861505f90ec
Patryk Smakosz <patryksmakosz1@gmail.com>
2025-02-10 15:01:41 +0100

    refactor(content): simplify homepage content and update ParallaxImg usage

--- components/content/ParallaxImg.vue
+++ components/content/ParallaxImg.vue
@@ -6,22 +6,14 @@
   </div>
 </template>
 
-<script setup>
-defineProps({
-  src: { type: String, required: true },
-  alt: { type: String, default: '' },
-  format: { type: String, default: 'webp' },
-  quality: { type: [String, Number], default: 70 },
-  loading: { type: String, default: 'lazy' },
-});
-</script>
+<script setup></script>
 
 <style lang="scss">
 .parallax-wrapper {
   height: 120%;
 }
 
-:deep(.parallax-content) {
+.parallax-content {
   width: 100%;
   height: 100%;
   object-fit: cover;

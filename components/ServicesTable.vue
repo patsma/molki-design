42bd68472a5fcb978531fa280e528d998df4eb39
Patryk Smakosz <patryksmakosz1@gmail.com>
2025-02-14 15:40:47 +0100

    feat(services): update ServicesTable styling and adjust table details

--- components/ServicesTable.vue
+++ components/ServicesTable.vue
@@ -13,20 +13,26 @@
             <thead>
               <tr>
                 <th class="p-4 bg-neutral-200"></th>
-                <th colspan="2" class="p-4 text-center bg-primary/80 text-neutral-100">
+                <th
+                  colspan="2"
+                  class="p-4 text-center bg-primary-light text-neutral-100 rounded-tl-2xl rounded-tr-2xl"
+                >
                   <div class="text-2xl font-semibold mb-2">Projekt Funkcjonalny</div>
                 </th>
-                <th colspan="2" class="p-4 text-center bg-primary text-neutral-100">
+                <th
+                  colspan="2"
+                  class="p-4 text-center bg-primary text-neutral-100 rounded-tl-2xl rounded-tr-2xl"
+                >
                   <div class="text-2xl font-semibold mb-2">Projekt Wykonawczy</div>
                 </th>
               </tr>
-              <tr class="border-b border-neutral-300">
+              <tr class="border-b border-neutral-400">
                 <th class="p-4 bg-neutral-200"></th>
-                <th class="p-4 text-center bg-primary/80 text-neutral-100">
+                <th class="p-4 text-center bg-primary-light text-neutral-100">
                   <div class="font-semibold">Podstawowy</div>
                   <div class="text-sm mt-1">od 60 zł/m²</div>
                 </th>
-                <th class="p-4 text-center bg-primary/80 text-neutral-100">
+                <th class="p-4 text-center bg-primary-light text-neutral-100">
                   <div class="font-semibold">Premium</div>
                   <div class="text-sm mt-1">od 120 zł/m²</div>
                 </th>
@@ -42,37 +48,53 @@
             </thead>
             <!-- Body -->
             <tbody>
-              <tr class="border-b border-neutral-300">
+              <tr class="border-b border-neutral-400">
                 <td class="p-4 bg-neutral-200 font-medium">Inwentaryzacja</td>
-                <td class="p-4 text-center">
-                  <Icon name="uil:check" class="w-6 h-6 text-primary mx-auto" />
+                <td class="p-4 text-center align-middle h-[4.5rem]">
+                  <div class="flex items-center justify-center h-full">
+                    <Icon name="uil:check" class="w-6 h-6 text-primary" />
+                  </div>
                 </td>
-                <td class="p-4 text-center">
-                  <Icon name="uil:check" class="w-6 h-6 text-primary mx-auto" />
+                <td class="p-4 text-center align-middle h-[4.5rem]">
+                  <div class="flex items-center justify-center h-full">
+                    <Icon name="uil:check" class="w-6 h-6 text-primary" />
+                  </div>
                 </td>
-                <td class="p-4 text-center">
-                  <Icon name="uil:check" class="w-6 h-6 text-primary mx-auto" />
+                <td class="p-4 text-center align-middle h-[4.5rem]">
+                  <div class="flex items-center justify-center h-full">
+                    <Icon name="uil:check" class="w-6 h-6 text-primary" />
+                  </div>
                 </td>
-                <td class="p-4 text-center">
-                  <Icon name="uil:check" class="w-6 h-6 text-primary mx-auto" />
+                <td class="p-4 text-center align-middle h-[4.5rem]">
+                  <div class="flex items-center justify-center h-full">
+                    <Icon name="uil:check" class="w-6 h-6 text-primary" />
+                  </div>
                 </td>
               </tr>
-              <tr class="border-b border-neutral-300">
+              <tr class="border-b border-neutral-400">
                 <td class="p-4 bg-neutral-200 font-medium">Wyburzenia/zabudowy ścian</td>
-                <td class="p-4 text-center">
-                  <Icon name="uil:check" class="w-6 h-6 text-primary mx-auto" />
+                <td class="p-4 text-center align-middle h-[4.5rem]">
+                  <div class="flex items-center justify-center h-full">
+                    <Icon name="uil:check" class="w-6 h-6 text-primary" />
+                  </div>
                 </td>
-                <td class="p-4 text-center">
-                  <Icon name="uil:check" class="w-6 h-6 text-primary mx-auto" />
+                <td class="p-4 text-center align-middle h-[4.5rem]">
+                  <div class="flex items-center justify-center h-full">
+                    <Icon name="uil:check" class="w-6 h-6 text-primary" />
+                  </div>
                 </td>
-                <td class="p-4 text-center">
-                  <Icon name="uil:check" class="w-6 h-6 text-primary mx-auto" />
+                <td class="p-4 text-center align-middle h-[4.5rem]">
+                  <div class="flex items-center justify-center h-full">
+                    <Icon name="uil:check" class="w-6 h-6 text-primary" />
+                  </div>
                 </td>
-                <td class="p-4 text-center">
-                  <Icon name="uil:check" class="w-6 h-6 text-primary mx-auto" />
+                <td class="p-4 text-center align-middle h-[4.5rem]">
+                  <div class="flex items-center justify-center h-full">
+                    <Icon name="uil:check" class="w-6 h-6 text-primary" />
+                  </div>
                 </td>
               </tr>
-              <tr class="border-b border-neutral-300">
+              <tr class="border-b border-neutral-400">
                 <td class="p-4 bg-neutral-200 font-medium">Układ funkcjonalny z opisami</td>
                 <td class="p-4 text-center">
                   <Icon name="uil:check" class="w-6 h-6 text-primary mx-auto" />
@@ -87,7 +109,7 @@
                   <Icon name="uil:check" class="w-6 h-6 text-primary mx-auto" />
                 </td>
               </tr>
-              <tr class="border-b border-neutral-300">
+              <tr class="border-b border-neutral-400">
                 <td class="p-4 bg-neutral-200 font-medium">
                   Lista zakupów (materiałów, mebli, dodatków)
                 </td>
@@ -95,7 +117,7 @@
                   <Icon name="uil:check" class="w-6 h-6 text-primary mx-auto" />
                 </td>
                 <td class="p-4 text-center">
-                  <Icon name="uil:check" class="w-6 h-6 text-primary mx-auto" />
+                  <Icon name="uil:times" class="w-6 h-6 text-neutral-500 mx-auto" />
                 </td>
                 <td class="p-4 text-center">
                   <Icon name="uil:check" class="w-6 h-6 text-primary mx-auto" />

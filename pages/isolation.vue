1c205138513d3a522d5e6a3872491064051b7e37
Patryk Smakosz <patryksmakosz1@gmail.com>
2025-02-15 14:36:16 +0100

    feat(isolation): dynamically render projects from collection

--- pages/isolation.vue
+++ pages/isolation.vue
@@ -1,41 +1,32 @@
 <template>
   <div class="content-grid py-20">
     <div class="spacer w-full h-[25vh] bg-red-400"></div>
-    <SquareGrid>
+
+    <SquareGrid v-if="projects?.length">
       <template #title>Nasze Realizacje</template>
       <template #subtitle>Zobacz nasze najnowsze projekty</template>
       <template #items>
         <SquareGridItem
-          to="/projects/project-1"
-          number="01"
-          title="Projekt wnętrza mieszkania"
-          location="Gdańsk"
-          year="2023"
-          image="/heroHome.jpg"
-        />
-        <SquareGridItem
-          to="/projects/project-2"
-          number="02"
-          title="Apartament nad morzem"
-          location="Sopot"
-          year="2023"
-          image="/heroHome.jpg"
-        />
-        <SquareGridItem
-          to="/projects/project-3"
-          number="03"
-          title="Dom jednorodzinny"
-          location="Gdynia"
-          year="2022"
-          image="/heroHome.jpg"
+          v-for="project in projects"
+          :key="project.id"
+          :to="`/projects/${project.slug}`"
+          :number="project.number"
+          :title="project.title"
+          :location="project.location"
+          :year="project.year"
+          :image="project.cover"
         />
       </template>
     </SquareGrid>
+
     <div class="spacer w-full h-[250vh] bg-teal-400"></div>
   </div>
 </template>
 
 <script setup>
-import SquareGrid from '~/components/SquareGrid.vue';
-import SquareGridItem from '~/components/SquareGridItem.vue';
+import { queryCollection } from '#imports';
+
+const { data: projects } = await useAsyncData('isolation-projects', () =>
+  queryCollection('projects').order('number', 'ASC').all()
+);
 </script>

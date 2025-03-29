609eeaac2a2d0160b94a71b3109446fa704f87ff
Patryk Smakosz <patryksmakosz1@gmail.com>
2025-03-29 15:15:35 +0100

    chore: team member achievement increase font size

--- components/Achievement.vue
+++ components/Achievement.vue
@@ -9,11 +9,11 @@
     data-scroll-stagger="0.2"
     data-scroll-order="4"
   >
-    <h3 v-if="$slots.title" class="h3-style font-bold text-primary mb-2">
+    <h3 v-if="$slots.title" class="h2-style font-bold text-primary mb-2">
       <slot name="title" mdc-unwrap="p" />
     </h3>
     <div class="w-12 h-0.5 bg-primary mx-auto mb-2"></div>
-    <p v-if="$slots.subtitle" class="body-small">
+    <p v-if="$slots.subtitle" class="">
       <slot name="subtitle" mdc-unwrap="p" />
     </p>
   </div>
